05cdfddc9db228dca1343498cb46c6de0d4ab0b0
ayberk-demir <hayberk.demir@smart-maple.com>
2024-10-14 10:05:08 +0300

    Aerial window config change

--- lua/kickstart/plugins/aerial.lua
+++ lua/kickstart/plugins/aerial.lua
@@ -3,6 +3,22 @@ return {
   config = function()
     require('aerial').setup {
       -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+      layout = {
+        max_width = 0.6,
+        min_width = 0.25,
+      },
+      filter_kind = {
+        "Class",
+        "Constructor",
+        "Enum",
+        "Function",
+        "Interface",
+        "Module",
+        "Method",
+        "Struct",
+        "Variable",
+        "Variables"
+      },
       on_attach = function(bufnr)
         -- Jump forwards/backwards with '{' and '}'
         vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
